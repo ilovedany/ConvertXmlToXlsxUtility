9885e33a5a5b13267bf8f4aea4f7cfc5f1d99e5c
Данил <dnazmut@mail.ru>
2025-03-02 23:15:43 +0500

    унифицирован код

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using Spire.Xls;
 using System.Xml;
 using System.Text;
+using ConvertXmlToXlsxUtility;
 class Program{
     static void Main(string[] args){
 
@@ -8,10 +9,7 @@ class Program{
 
         XmlDocument xDoc = new XmlDocument();
         
-        xDoc.Load(args[0]); //первый параметр - путь к xml файлу
-
-
-
+        xDoc.Load("C:/XmlFiles/12.xml"); //первый параметр - путь к xml файлу
 
         XmlElement? xRoot = xDoc.DocumentElement;
 
@@ -19,71 +17,48 @@ class Program{
 
         Worksheet worksheet = workbook.Worksheets[0];
 
-        
-
-        worksheet.Range[1,1].Value = "ТабНом";
-        worksheet.Range[1,2].Value = "ФИО";
-        worksheet.Range[1,3].Value = "УчебноеЗаведение";
-        worksheet.Range[1,4].Value = "Специальность";
-        worksheet.Range[1,5].Value = "ПрежняяДолжность";
-        worksheet.Range[1,6].Value = "ДатаНазначенияНаПрежнююДолжность";
-        worksheet.Range[1,7].Value = "НоваяДолжность";
-        worksheet.Range[1,8].Value = "ДатаНазначения";
+        WorksheetColumns worksheetColumns = new WorksheetColumns();
 
 
-        CellStyle style = workbook.Styles.Add("newStyle");
-        style.Font.IsBold = true;
-        worksheet.Range[1, 1, 1, 8].Style = style;
 
-        int countLine=1;
         int lineCount = 0;
+        int countLine = 1;
+        string[] columns = {"ТабНом","ФИО","УчебноеЗаведение","Специальность","ПрежняяДолжность","ДатаНазначенияНаПрежнююДолжность","НоваяДолжность","ДатаНазначения"};
+
+        string[] columns2 = new string[columns.Length - 4];
+        Array.Copy(columns, 4, columns2, 0, columns2.Length); 
+
+        worksheetColumns.WorkingWithXml(workbook,countLine,columns);
+       
         if (xRoot != null)
         {
-
             foreach (XmlElement xnode in xRoot)
             {
                 if (xnode.Name == "LIST_G_KADR")
                 {
-
                     foreach (XmlElement userNode in xnode.GetElementsByTagName("G_KADR"))
                     {
                         countLine++;
-
-                        worksheet.Range[countLine,1].Value =  userNode["ТабНом"]?.InnerText;
-                        
-                        worksheet.Range[countLine,2].Value = userNode["ФИО"]?.InnerText;
-                        worksheet.Range[countLine,3].Value = userNode["УчебноеЗаведение"]?.InnerText;
-                        worksheet.Range[countLine,4].Value = userNode["Специальность"]?.InnerText;
+                        worksheetColumns.WorkingWithXml(userNode, workbook, countLine, columns,0);
 
                         foreach (XmlElement profList in userNode.GetElementsByTagName("LIST_G_PROF"))
                         {
                             foreach (XmlElement userList in profList.GetElementsByTagName("G_PROF"))
                             {
-                                worksheet.Range[countLine,5].Value =  userList["ПрежняяДолжность"]?.InnerText;
-                                worksheet.Range[countLine,6].Value =  userList["ДатаНазначенияНаПрежнююДолжность"]?.InnerText;
-                                worksheet.Range[countLine,7].Value =  userList["НоваяДолжность"]?.InnerText;
-                                worksheet.Range[countLine,8].Value =  userList["ДатаНазначения"]?.InnerText;
-
+                                worksheetColumns.WorkingWithXml(userList, workbook, countLine, columns2,4);
+                                countLine++;
                                 lineCount++;
-                                countLine++; 
+
                             }
                         }
                         countLine--;
-                        worksheet.Range[countLine-lineCount+1,1,countLine,1].Merge();
-                        worksheet.Range[countLine-lineCount+1,2,countLine,2].Merge(); 
-                        worksheet.Range[countLine-lineCount+1,3,countLine,3].Merge(); 
-                        worksheet.Range[countLine-lineCount+1,4,countLine,4].Merge();  
-                        worksheet.AllocatedRange.AutoFitColumns();
-                        workbook.SaveToFile(args[1], ExcelVersion.Version2016); //второй параметр - место сохранения xlsx файла
+                        worksheetColumns.WorkingWithXml(workbook,countLine-lineCount+1,countLine,4);
                         lineCount = 0;
-                        
-                    }
-                
+                    }     
                 }
             }
         }
-
+        worksheet.AllocatedRange.AutoFitColumns();
+        workbook.SaveToFile("C:/XmlFiles/dany.xlsx", ExcelVersion.Version2016);
     }
 }
-
-    
